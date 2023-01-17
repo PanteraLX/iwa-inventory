2b480f9d4905cb2757392ccc0641424c42c2f257
fabioklr <fabiokeller@bluewin.ch>
2023-01-17 16:32:52 +0100

    first styles for detail view

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from app.models import InventoryItem
 
+inventory_item_css_class = 'shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+
 class InventoryItemForm(forms.ModelForm):
     class Meta:
         model = InventoryItem
         fields = ['name', 'description', 'quantity', 'position', 'producer']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': inventory_item_css_class}),
+            'description': forms.Textarea(attrs={'class': 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500'}),
+            'quantity': forms.NumberInput(attrs={'class': inventory_item_css_class}),
+            'position': forms.TextInput(attrs={'class': inventory_item_css_class}),
+            'producer': forms.TextInput(attrs={'class': inventory_item_css_class}),
+        }
+
 
 
 

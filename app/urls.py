61f1592de9695faea4590cd092530e4b33ab90ff
fabioklr <fabiokeller@bluewin.ch>
2023-01-17 13:02:34 +0100

    Detail and update inventory item

--- app/urls.py
+++ app/urls.py
@@ -20,5 +20,7 @@ urlpatterns = [
     path('archive/<int:id>/', views.inventory_item_archive, name='archive_inventory_item'),
     path('unarchive/<int:id>/', views.inventory_item_unarchive, name='unarchive_inventory_item'),
     path('complete/', complete_inventory_items_list, name='complete_inventory_items_list'),
+    path('inventory_item/<int:pk>/', views.InventoryItemDetailView.as_view(), name='inventory_item_detail'),
+    path('inventory_item/<int:pk>/update', views.UpdateView, name='inventory_item_update'),
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from app.models import InventoryItem
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView, DetailView
 from app.forms import InventoryItemForm
 
 # Switching the 'active' variable of an InventoryItem instance to False
@@ -26,6 +26,29 @@ class InventoryItemListView(ListView):
     model = InventoryItem
     template_name = 'app/inventory_item_list.html'
     context_object_name = 'inventory_items'
+
+# A detail view of a single InventoryItem instance
+
+class InventoryItemDetailView(DetailView):
+    model = InventoryItem
+    template_name = 'app/inventory_item_detail.html'
+    context_object_name = 'inventory_item'
+
+# A function-based view to update an InventoryItem instance
+
+def UpdateView(request, pk):
+    inventory_item = InventoryItem.objects.get(id=pk)
+    form = InventoryItemForm(request.POST or None, instance=inventory_item)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            inventory_item = form.save(commit=False)
+            inventory_item.save()
+            return redirect('inventory_item_detail', pk=inventory_item.pk)
+    else:
+        return render(request,
+                        'app/inventory_item_update.html',
+                        {'form': form})
 
 # A function-based view to create a new InventoryItem
 

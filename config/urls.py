6dbafc8fa582233b820b4aa8b1b2f5e12da08745
fabioklr <fabiokeller@bluewin.ch>
2023-01-19 09:31:33 +0100

    Update2

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,3 +28,5 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

bac552405bde548ce1e535453140b7be54c3a3cc
Samuel Heinzmann <samuel.heinzmann@vtg.admin.ch>
2023-01-26 13:05:58 +0100

    Use categories in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from app.models import InventoryItem, Order
+from app.models import InventoryItem, Order, Category
 
 admin.site.register(InventoryItem)
 admin.site.register(Order)
+admin.site.register(Category)
 
 

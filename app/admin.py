de4c5c64668eb7916cb2410d92e21c3f75301a1a
Samuel Heinzmann <samuel.heinzmann@vtg.admin.ch>
2023-02-03 08:15:57 +0100

    Update admin view for lends

--- app/admin.py
+++ app/admin.py
@@ -4,12 +4,11 @@ from app.models import InventoryItem, Lend, SingleInventoryItem, InventoryItemIm
 # In the Admin UI create a filter for the lend model to only show lends that have not yet been returned
 class LendAdmin(admin.ModelAdmin):
     list_filter = ('returned',)
-    list_display = ('returned',)
+    list_display = ('__str__', 'started_at', 'ended_at', 'returned',)
+    
 
 admin.site.register(InventoryItem)
 admin.site.register(Lend, LendAdmin)
 admin.site.register(SingleInventoryItem)
 admin.site.register(InventoryItemImage)
 admin.site.register(Category)
-
-
